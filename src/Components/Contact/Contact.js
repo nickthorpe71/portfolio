c3641590015ead5c4015dee88376c2775af0f075
nickthorpe71 <nickthorpe71@gmail.com>
2020-08-18 15:12:01 -0500

    adds contact form functionality

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './Contact.css';
 
 export default function Contact(props) {
 
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [subject, setSubject] = useState('');
+  const [message, setMessage] = useState('');
+  const [buttonText, setButtonText] = useState('Send Message');
+  const [sent, setSentBool] = useState(false);
+
+
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(e.target.contactName.value);
+
+    setButtonText('Sending...');
+
+    let data = {
+      name,
+      email,
+      subject,
+      message
+    };
+
+    axios.post('API_URI', data)
+      .then(res => {
+        setSentBool(true);
+        resetForm();
+      })
+      .catch(() => {
+        console.log('Message not sent');
+      });
+  }
+
+  function resetForm() {
+    setName('');
+    setEmail('');
+    setSubject('');
+    setMessage('');
+    setButtonText('Send Message');
   }
 
   return (
@@ -22,32 +56,38 @@ export default function Contact(props) {
       </div>
       <div className="row section-body">
         <div className="column">
+          {/* form start */}
           <form method="post" id="contactForm" name="contactForm" onSubmit={handleSubmit}>
             <fieldset>
               <div>
                 <label htmlFor="contactName">Name <span className="required">*</span></label>
-                <input type="text" size={35} id="contactName" name="contactName" required />
+                <input type="text" size={35} id="contactName" name="contactName"
+                  onChange={e => setName(e.target.value)} required />
               </div>
               <div>
                 <label htmlFor="contactEmail">Email <span className="required">*</span></label>
-                <input type="email" size={35} id="contactEmail" name="contactEmail" required />
+                <input type="email" size={35} id="contactEmail" name="contactEmail"
+                  onChange={e => setEmail(e.target.value)} required />
               </div>
               <div>
                 <label htmlFor="contactSubject">Subject</label>
-                <input type="text" size={35} id="contactSubject" name="contactSubject" />
+                <input type="text" size={35} id="contactSubject" name="contactSubject"
+                  onChange={e => setSubject(e.target.value)} />
               </div>
               <div>
                 <label htmlFor="contactMessage">Message <span className="required">*</span></label>
-                <textarea cols={50} rows={15} id="contactMessage" name="contactMessage" defaultValue={""} required />
+                <textarea cols={50} rows={15} id="contactMessage" name="contactMessage"
+                  onChange={e => setMessage(e.target.value)} defaultValue={""} required />
               </div>
               <div>
-                <button className="submit">Submit</button>
+                <button type="submit" className="submit">{buttonText}</button>
                 <span id="image-loader">
                   <img alt="" src="images/loader.gif" />
                 </span>
               </div>
             </fieldset>
           </form>
+          {/* form end */}
           <div id="message-warning"> Error boy</div>
           <div id="message-success">
             <i className="fa fa-check" />Your message was sent, thank you!<br />
